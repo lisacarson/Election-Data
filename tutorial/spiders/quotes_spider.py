92559b59e83e7c384c0f0e41bf9d71263d9c0312
carsln <lcarson35@hotmail.com>
2016-11-28 21:13:15 -0500

    Saving scraped democrat data in database

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import hashlib
 from bs4 import BeautifulSoup
 from database import Database
 
@@ -23,15 +24,44 @@ class QuotesSpider(scrapy.Spider):
             f.write(response.body)
         self.log('Saved file %s' % filename)
         soup = BeautifulSoup(response.body, 'html.parser')
+        year = response.url.split("/")[3]
+        newYear = year.split("-")[0]
+        print(newYear)
         for article in soup.find_all('article'):
             # print(len(list(articles)))
-            self.parseArticle(article)
+            self.parseArticle(article, page, newYear)
 
-    def parseArticle(self, articles):
+
+    def parseArticle(self, articles, state, year):
         if articles.get('id') != None:
-            print(articles.select('.type-democrat .results-popular'))
+            #print(articles.select('.type-democrat .results-popular'))
             democratPopularResult = articles.select('.type-democrat .results-popular')
-            # votes = {
-            #    "democrat": democratPopularResult
-            # }
-            self.database.set(articles.get('id') + " democrat", str(democratPopularResult))
+            print(len(democratPopularResult))
+            if (len(democratPopularResult) > 0):
+                newResult = democratPopularResult[0].string.replace(",", "")
+                #print(int(newResult))
+                counties = articles.select('h4')
+                newCounties = counties[0].string
+                party = "democrat"
+                dbObject = {
+                    "state": state,
+                    "year": year,
+                    "newCounties": newCounties,
+                    "party": party,
+                    "result": int(newResult)
+                }
+                print(dbObject)
+
+                uniqueId = state + year + newCounties + party
+                print(uniqueId)
+
+                m = hashlib.md5()
+                m.update(uniqueId)
+                newId = m.hexdigest()
+                print(newId)
+
+                #print(newCounties)
+                # votes = {
+                #    "democrat": democratPopularResult
+                # }
+                self.database.set(newId, dbObject)
